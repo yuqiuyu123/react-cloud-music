55d97409b9d0288845da8cf1f8634abc78686dd7
yuqiuyu <yuqiuyu@qq.com>
2021-06-25 10:11:42 +0800

    dd

--- src/api/utils.js
+++ src/api/utils.js
@@ -40,3 +40,15 @@ export const filterIdx = name => {
   }
   return null;
 };
+
+// 处理歌手列表拼接歌手名字
+export const getName = list => {
+  let str = "";
+  list.map ((item, index) => {
+    str += index === 0 ? item.name : "/" + item.name;
+    return item;
+  });
+  return str;
+};
+
+export const isEmptyObject = obj => !obj || Object.keys (obj).length === 0;
---
+++ src/application/Album/store/constants.js
@@ -0,0 +1,3 @@
+export const CHANGE_CURRENT_ALBUM = 'album/CHANGE_CURRENT_ALBUM';
+export const CHANGE_ENTER_LOADING = 'album/CHANGE_ENTER_LOADING';
+export const CLEAR_DATA = 'album/CLEAR_DATA';
